2986703ab6aef5d8c98658fa9466c9f05da453ed
EmmaMaltseva <emmamaltseva0@gmail.com>
2023-08-29 21:10:26 +0300

    Добавление товаров в корзину

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id: 1,
+          title: 'Стул бежевый',
+          img: 'chair_gray.avif',
+          desc: 'Кресло Kilanda светло-бежевого цвета с коричневым каркасом',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.avif',
+          desc: 'Выдвижной стол, белый, 82 5/8/113 3/4x41 3/8 "',
+          category: 'tables',
+          price: '149.00'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.avif',
+          desc: 'Диван, окрашенный в бежевый цвет',
+          category: 'sofas',
+          price: '449.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'lamp.avif',
+          desc: 'Торшер, черный/белый, 59 "',
+          category: 'sofas',
+          price: '449.99'
+        }, 
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair_white.avif',
+          desc: 'Вращающееся кресло + подушка, белый/темно-серый',
+          category: 'sofas',
+          price: '449.99'
+        }
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this) /*дает возможность в методе addToOrder рабоатть с состояниями*/
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header orders={this.state.orders}/> {/*Передаем массив orders в шапку для вывода на экран в корзине*/}
+        <Items items={this.state.items} onAdd={this.addToOrder}/> {/*onAdd - новый пропс, свойство сами сделали, передаем его*/}
+        <Footer />
+      </div>
+    );
+  }
+
+  addToOrder(item) {
+    this.setState({ orders: [...this.state.orders, item] })
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa"
+import Order from './Order';
 
-export default function Header() {
+export default function Header(props) { /*прописываем, что Header принимает propsы, тк это функция, а не класс */
+let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
       <div>
@@ -10,6 +14,16 @@ export default function Header() {
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/> {/*добавляем класс active, если состояние cartOpen true*/}
+      
+        {cartOpen && (
+          <div className='shop-cart'>
+            {props.orders.map(el => (
+              <Order key={el.id} item={el}/>
+            ))}
+          </div>
+        )} {/*Окно при нажатии на корзину */}
+      
       </div>
       <div className="presentation"></div>
     </header>
